20bce44430e2e8fbffa2c63f1154a3282ed7da8a
Andre Alves <40440560+aalves932@users.noreply.github.com>
2024-02-01 14:30:40 -0300

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,7 @@
 dotnet publish
+touch ./docs/.nojekyll
+cp -Rf ./algoritmos/algo/bin/Release/net8.0/publish/wwwroot/ ./docs/
+
+FILE="./docs/index.html"
+cp "$FILE" "$FILE.bak"
+sed -i '' 's|<base href="/" />|<base href="https://aalves932.github.io/algo/" />|' "$FILE"
