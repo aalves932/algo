08fa71b925c92eede8cced509ac0a92eb2d860b7
Andre Alves <40440560+aalves932@users.noreply.github.com>
2024-02-05 17:17:44 -0300

    Deploy from CLI

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,6 @@ cp -Rf ./algoritmos/algo/bin/Release/net8.0/publish/wwwroot/ ./docs/
 FILE="./docs/index.html"
 cp "$FILE" "$FILE.bak"
 sed -i '' 's|<base href="/" />|<base href="https://aalves932.github.io/algo/" />|' "$FILE"
+
+git commit -am "Deploy from CLI"
+git push
